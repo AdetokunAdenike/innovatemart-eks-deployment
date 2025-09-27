f55a2074afe067269b299dbcb948f1396923ddc3
AdetokunAdenike <adetokunadenike@gmail.com>
2025-09-27 11:55:00 +0100

    Fix: Cleaned terraform state drift

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.cluster_name}-cluster-role"
+  name               = "${var.cluster_name}-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
 }
 
@@ -25,7 +25,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceContr
 }
 
 resource "aws_iam_role" "eks_node_role" {
-  name = "${var.cluster_name}-node-role"
+  name               = "${var.cluster_name}-node-role"
   assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role_policy.json
 }
 
@@ -53,40 +53,4 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
 resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_role.name
-}
-
-# Read-only developer user
-resource "aws_iam_user" "developer" {
-  name = "bedrock-dev-user"
-}
-
-resource "aws_iam_access_key" "developer" {
-  user = aws_iam_user.developer.name
-}
-
-data "aws_iam_policy_document" "developer_readonly" {
-  statement {
-    sid    = "ReadOnlyAccess"
-    effect = "Allow"
-    actions = [
-      "eks:Describe*",
-      "eks:List*",
-      "ec2:Describe*",
-      "rds:Describe*",
-      "dynamodb:ListTables",
-      "dynamodb:DescribeTable",
-      "elasticache:Describe*"
-    ]
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_policy" "developer_readonly" {
-  name   = "BedrockDevUserReadOnlyPolicy"
-  policy = data.aws_iam_policy_document.developer_readonly.json
-}
-
-resource "aws_iam_user_policy_attachment" "developer_readonly" {
-  user       = aws_iam_user.developer.name
-  policy_arn = aws_iam_policy.developer_readonly.arn
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,17 +17,3 @@ output "node_group_instance_type" {
 output "node_group_desired_capacity" {
   value = aws_eks_node_group.main.scaling_config[0].desired_size
 }
-
-output "bedrock_dev_user_access_key_id" {
-  value     = aws_iam_access_key.developer.id
-  sensitive = true
-}
-
-output "bedrock_dev_user_secret_access_key" {
-  value     = aws_iam_access_key.developer.secret
-  sensitive = true
-}
-
-output "bedrock_dev_user_arn" {
-  value = aws_iam_user.developer.arn
-}
