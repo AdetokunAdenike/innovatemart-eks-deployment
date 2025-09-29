7d325dc5a92534cac0b1ef383f8c0f0ef41edb7e
AdetokunAdenike <adetokunadenike@gmail.com>
2025-09-29 18:54:56 +0100

    upscale

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -30,11 +30,11 @@ resource "aws_eks_node_group" "main" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 2
+    max_size     = 4
     min_size     = 1
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.medium"]
 
   tags = {
     Name = "${var.cluster_name}-nodegroup"
